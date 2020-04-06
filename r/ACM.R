c4939ed47f6afafd67340608d6d30e4dad57ae78
Juliette Bassnagel <juliette.bassnagel@mfglabs.com>
2020-04-06 12:14:49 +0200

    ACM vehic, usagers, lieux

--- r/ACM.R
+++ r/ACM.R
@@ -22,8 +22,6 @@ caract_pieton <- usag %>%
          int = if_else(int != "Hors intersection", "Intersection", "Hors intersection")) %>% 
   select(lum, agg, int, atm, zone, grav)
 
-rm(pietons)
-
 # acm
 acm <- MCA(caract_pieton, quali.sup = 6, graph = F)
 plot(acm, 
@@ -36,13 +34,13 @@ plot(acm,
 caract_grav <- usag %>% 
   inner_join(caract) %>% 
   mutate(grav = factor(grav, levels = c("Tué", "Blessé hospitalisé", "Blessé léger", "Indemne")),
-         lum = factor(lum, levels = c("Crépuscule ou aube", "Plein jour", "Nuit avec éclairage public allumé", "Nuit avec éclairage public non allumé", "Nuit sans éclairage public")),
-         int = if_else(int != "Hors intersection", "Intersection", "Hors intersection")) %>% 
+         int = if_else(int != "Hors intersection", "Intersection", "Hors intersection"),
+         zone = if_else(dep %in% c(75, 77, 78, 91, 92, 93, 94, 95), "IDF", "Province")
+         ) %>% 
   arrange(Num_Acc, grav) %>% 
   group_by(Num_Acc) %>% 
   mutate(rank = seq(1:n())) %>% 
   filter(rank == 1) %>% 
-  mutate(zone = if_else(dep %in% c(75, 77, 78, 91, 92, 93, 94, 95), "IDF", "Province")) %>% 
   ungroup() %>% 
   select(lum, agg, int, atm, zone, grav)
 
@@ -54,14 +52,142 @@ plot(acm,
 
 # Lieu de l'accident ------------------------------------------------------
 
+# Localisation de l'accident si au moins un piéton est impliqué
 
-# Usagers -----------------------------------------------------------------
+# Préparation de la donnée
 
+lieux_pieton <- usag %>% 
+  inner_join(lieux) %>% 
+  filter(Num_Acc %in% pietons$Num_Acc) %>% 
+  mutate(plan = if_else(plan != "Partie rectiligne", "En courbe", "Partie rectiligne")) %>% 
+  select(catr, #circ, 
+         prof, plan, surf, grav)
+
+# acm
+acm <- MCA(lieux_pieton, quali.sup = 5, graph = F)
+plot(acm, 
+     quali.sup = 5, 
+     invisible = 'ind')
+
+# Gravité de l'accident expliqué par sa localisation
+
+# Préparation de la donnée
+lieux_grav <- usag %>% 
+  inner_join(lieux) %>% 
+  mutate(grav = factor(grav, levels = c("Tué", "Blessé hospitalisé", "Blessé léger", "Indemne")),
+         plan = if_else(plan != "Partie rectiligne", "En courbe", "Partie rectiligne")) %>% 
+  arrange(Num_Acc, grav) %>% 
+  group_by(Num_Acc) %>% 
+  mutate(rank = seq(1:n())) %>% 
+  filter(rank == 1) %>% 
+  ungroup() %>% 
+  select(catr, #circ, 
+         prof, plan, surf, grav)
+
+# acm
+acm <- MCA(lieux_grav, quali.sup = 5, graph = F)
+plot(acm, 
+     quali.sup = 5, 
+     invisible = 'ind')
 
 # Véhicules ---------------------------------------------------------------
 
+# Caractéristiques du véhicule si au moins un piéton est impliqué
+
+# Préparation de la donnée
+vehic_pieton <- usag %>% 
+  inner_join(vehic) %>% 
+  filter(Num_Acc %in% pietons$Num_Acc) %>% 
+  mutate(obs = if_else(!is.na(obs), "Obstacle", "Pas d'obstacle"),
+         catv = case_when(
+           catv %in% c("Tramway", "Autobus", "Train", "Autocar") ~ "Transport en commun",
+           stri_detect_fixed(catv, "Scooter") ~ "2 roues motorisé",
+           stri_detect_fixed(catv, "Motocyclette") ~ "2 roues motorisé",
+           catv == "Cyclomoteur < 50cm3" ~ "2 roues motorisé",
+           stri_detect_fixed(catv, "Tracteur") ~ "Tracteur",
+           stri_detect_fixed(catv, "PL") ~ "Engin spécial",
+           catv == "Engin spécial" ~ "Engin spécial",
+           stri_detect_fixed(catv, "Quad") ~ "Quad",
+           catv %in% c("VL seul", "Voiturette") ~ "Voiture",
+           stri_detect_fixed(catv, "VU") ~ "Véhicule utilitaire",
+           catv == "Bicyclette" ~ "Vélo",
+           catv == "Autre véhicule" ~ "Autre véhicule")
+         ) %>% 
+  # suppression des quads car comportement particulier
+  filter(catv != 'Quad') %>% 
+  select(catv, obs, grav)
+
+# acm
+acm <- MCA(vehic_pieton, quali.sup = 3, graph = F)
+plot(acm, 
+     quali.sup = 3, 
+     invisible = 'ind')
+
+# Gravité de l'accident expliqué par les caractéristiques du véhicule
+
+# Préparation de la donnée
+vehic_grav <- usag %>% 
+  inner_join(vehic) %>% 
+  mutate(grav = factor(grav, levels = c("Tué", "Blessé hospitalisé", "Blessé léger", "Indemne")),
+         obs = if_else(!is.na(obs), "Obstacle", "Pas d'obstacle"),
+         catv = case_when(
+           catv %in% c("Tramway", "Autobus", "Train", "Autocar") ~ "Transport en commun",
+           stri_detect_fixed(catv, "Scooter") ~ "2 roues motorisé",
+           stri_detect_fixed(catv, "Motocyclette") ~ "2 roues motorisé",
+           catv == "Cyclomoteur < 50cm3" ~ "2 roues motorisé",
+           stri_detect_fixed(catv, "Tracteur") ~ "Tracteur",
+           stri_detect_fixed(catv, "PL") ~ "Engin spécial",
+           catv == "Engin spécial" ~ "Engin spécial",
+           stri_detect_fixed(catv, "Quad") ~ "Quad",
+           catv %in% c("VL seul", "Voiturette") ~ "Voiture",
+           stri_detect_fixed(catv, "VU") ~ "Véhicule utilitaire",
+           catv == "Bicyclette" ~ "Vélo",
+           catv == "Autre véhicule" ~ "Autre véhicule")
+         ) %>% 
+  arrange(Num_Acc, grav) %>% 
+  group_by(Num_Acc) %>% 
+  mutate(rank = seq(1:n())) %>% 
+  filter(rank == 1) %>% 
+  ungroup() %>% 
+  filter(catv != 'Quad') %>%
+  select(catv, obs, grav)
+
+# acm
+acm <- MCA(vehic_grav, quali.sup = 3, graph = F)
+plot(acm, 
+     quali.sup = 3, 
+     invisible = 'ind')
+
+# Usagers -----------------------------------------------------------------
+
+usagers <- usag %>% 
+  mutate(age = 2018 - an_nais,
+         groupe_age = case_when(
+           age <= 10 ~ "0-10",
+           age > 10 & age <=20 ~ "11-20",
+           age > 20 & age <=30 ~ "21-30",
+           age > 30 & age <=40 ~ "31-40",
+           age > 40 & age <=50 ~ "41-50",
+           age > 50 & age <=60 ~ "51-60",
+           age > 60 & age <=70 ~ "61-70",
+           age > 70 & age <=80 ~ "71-80",
+           age > 80 & age <=90 ~ "81-90",
+           age > 90 ~ ">90")) %>% 
+  select(catu, sexe, trajet, groupe_age, utilisation_equipement_secu, grav)
+
+# acm
+acm <- MCA(usagers, quali.sup = 6, graph = F)
+plot(acm, 
+     quali.sup = 6, 
+     invisible = 'ind')
 
 # Global ------------------------------------------------------------------
 
+
+# Typologies d'accidents
+
+
+
+# Typologie d'individus
 
 
