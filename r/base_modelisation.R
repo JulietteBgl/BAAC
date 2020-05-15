c0412cee5773fa2d56d2d0ecfe7216b86209b5c6
Juliette Bassnagel <juliette.bassnagel@mfglabs.com>
2020-05-15 17:03:22 +0200

    factors recoding

--- r/base_modelisation.R
+++ r/base_modelisation.R
@@ -10,7 +10,7 @@ source("r/data.R")
 
 global_acc <- usag %>% 
   inner_join(caract) %>% 
-  mutate(grav = factor(grav, levels = c("Tué", "Blessé hospitalisé", "Blessé léger", "Indemne")),
+  mutate(grav = factor(grav, levels = c("Tué", "Blessé hospitalisé", "Blessé léger", "Indemne")), #pas de na
          int = if_else(int != "Hors intersection", "Intersection", "Hors intersection"),
          zone = if_else(dep %in% c(75, 77, 78, 91, 92, 93, 94, 95), "IDF", "Province")
   ) %>% 
@@ -32,7 +32,10 @@ global_acc <- usag %>%
            age > 55 & age <=65 ~ "56-65",
            age > 65 ~ "> 65"),
          obs = if_else(!is.na(obs), "Obstacle", "Pas d'obstacle"),
-         plan = if_else(plan != "Partie rectiligne", "En courbe", "Partie rectiligne"),
+         plan = ifelse(plan != "Partie rectiligne" & !is.na(plan), 
+                        yes = "En courbe", 
+                        no = as.character(plan)
+                        ), 
          catv = case_when(
            catv %in% c("Tramway", "Autobus", "Train", "Autocar") ~ "Transport en commun",
            stri_detect_fixed(catv, "Scooter") ~ "2 roues motorisé",
@@ -46,18 +49,16 @@ global_acc <- usag %>%
            stri_detect_fixed(catv, "VU") ~ "Véhicule utilitaire",
            catv == "Bicyclette" ~ "Vélo",
            catv == "Autre véhicule" ~ "Autre véhicule"),
-         prof = if_else(prof %in% c("Bas de côte", "Sommet de côte", "Pente"), "Pente", "Plat")
+         prof = ifelse(prof != "Plat" & !is.na(prof), 
+                        yes = "Pente", 
+                        no = as.character(prof)) 
   ) %>% 
-  filter(catv != 'Quad' &
-           !is.na(atm) &
-           !is.na(surf) &
-           !is.na(prof) &
-           !is.na(plan) &
-           !is.na(groupe_age) &
-           #focus sur les routes
-           catr %in% c("Autoroute", "Route Départementale", "Voie Communale", "Route Nationale")
-  ) %>% 
-  ungroup() 
+  select(-rank)
+
+names(global_acc)
+sapply(global_acc, function(x) sum(is.na(x)))
+prop.table(table(global_acc$plan, useNA = "always"))*100
+prop.table(table(global_acc$prof, useNA = "always"))*100
 
 # write csv
 write.csv(global_acc, "outputs/accidents_2017_alldata.csv")
@@ -67,46 +68,90 @@ write.csv(global_acc, "outputs/accidents_2017_alldata.csv")
 # par individus
 
 global_ind <- usag %>% 
-  inner_join(vehic) %>% 
-  inner_join(lieux) %>% 
-  inner_join(caract) %>%
-  mutate(int = if_else(int != "Hors intersection", "Intersection", "Hors intersection"),
+  left_join(vehic) %>% 
+  left_join(lieux) %>% 
+  left_join(caract) %>%
+  group_by(Num_Acc) %>% 
+  mutate(nb_pers_impliquées = n()) %>%
+  ungroup() %>% 
+  mutate(int = if_else(int != "Hors intersection", true = "Intersection", false = "Hors intersection"), #pas de na
          zone = if_else(dep %in% c(75, 77, 78, 91, 92, 93, 94, 95), "IDF", "Province"),
          age = 2017 - an_nais,
          groupe_age = case_when(
            age <= 16 ~ "0-16",
-           age > 16 & age <=25 ~ "17-25",
-           age > 25 & age <=35 ~ "26-35",
+           age > 16 & age <=21 ~ "17-21",
+           age > 21 & age <=35 ~ "22-35",
            age > 35 & age <=45 ~ "36-45",
            age > 45 & age <=55 ~ "46-55",
-           age > 55 & age <=65 ~ "56-65",
-           age > 65 ~ "> 65"),
+           age > 55 & age <=70 ~ "56-70",
+           age > 70 ~ "> 70"),
          obs = if_else(!is.na(obs), "Obstacle", "Pas d'obstacle"),
-         plan = if_else(plan != "Partie rectiligne", "En courbe", "Partie rectiligne"),
-         catv = case_when(
-           catv %in% c("Tramway", "Autobus", "Train", "Autocar") ~ "Transport en commun",
-           stri_detect_fixed(catv, "Scooter") ~ "2 roues motorisé",
-           stri_detect_fixed(catv, "Motocyclette") ~ "2 roues motorisé",
-           catv == "Cyclomoteur < 50cm3" ~ "2 roues motorisé",
-           stri_detect_fixed(catv, "Tracteur") ~ "Tracteur",
-           stri_detect_fixed(catv, "PL") ~ "Engin spécial",
-           catv == "Engin spécial" ~ "Engin spécial",
-           stri_detect_fixed(catv, "Quad") ~ "Quad",
-           catv %in% c("VL seul", "Voiturette") ~ "Voiture",
-           stri_detect_fixed(catv, "VU") ~ "Véhicule utilitaire",
+         plan = ifelse(plan != "Partie rectiligne" & !is.na(plan), 
+                              yes = "En courbe", 
+                              no = as.character(plan)
+         ),
+         cat_vehic = case_when(
            catv == "Bicyclette" ~ "Vélo",
-           catv == "Autre véhicule" ~ "Autre véhicule"),
-         prof = if_else(prof %in% c("Bas de côte", "Sommet de côte", "Pente"), "Pente", "Plat")
-  ) %>% 
-  filter(catv != 'Quad' &
-           !is.na(atm) &
-           !is.na(surf) &
-           !is.na(prof) &
-           !is.na(plan) &
-           !is.na(groupe_age) &
-           #focus sur les routes
-           catr %in% c("Autoroute", "Route Départementale", "Voie Communale", "Route Nationale")
-  ) 
+           stri_detect_fixed(catv, "Scooter") & catv != "Scooter > 125 cm3" ~ "2 roues peu puissants",
+           catv == "Cyclomoteur < 50cm3" ~ "2 roues peu puissants",
+           stri_detect_fixed(catv, "Motocyclette") & catv != "Motocyclette > 125 cm3" ~ "2 roues peu puissants",
+           catv %in% c("Motocyclette > 125 cm3", "Scooter > 125 cm3") ~ "2 roues > 125 cm3",
+           catv %in% c("VL seul", "Voiturette", "VU") ~ "Voiture",
+           stri_detect_fixed(catv, "VU ") ~ "Voiture",
+           stri_detect_fixed(catv, "PL") ~ "Poids lourds",
+           catv == "Engin spécial" ~ "Poids lourds",
+           stri_detect_fixed(catv, "Quad") ~ "Autre véhicule",
+           stri_detect_fixed(catv, "Tracteur") ~ "Autre véhicule",
+           catv %in% c("Tramway", "Autobus", "Train", "Autocar") ~ "Autre véhicule",
+           catv == "Autre véhicule" ~ "Autre véhicule"
+           ),
+         prof = ifelse(prof != "Plat" & !is.na(prof), 
+                       yes = "Pente", 
+                       no = as.character(prof)),
+         trajet = case_when(
+           trajet %in% c("Domicile – travail", "Utilisation professionnelle") ~ "Pro",
+           trajet %in% c("Domicile – école", "Courses – achats", "Promenade – loisirs", "Autre") ~ "Non Pro"
+         ),
+         lum = ifelse(lum == "Plein jour", "Jour", 
+                          ifelse(lum == "Nuit avec éclairage public allumé", "Nuit avec éclairage",
+                                                   "Nuit sans éclairage")),
+         meteo = ifelse(atm == "Normale", "Condition météo normale", "Mauvaise condition météo"),
+         surf = ifelse(surf != "Normale", "Surface glissante", "Surface normale"),
+         catu = ifelse(
+           catu == "Piéton en roller ou en trottinette", "Piéton", as.character(catu)
+         ),
+         choc = case_when(
+           stri_detect_fixed(choc, "Avant") ~ "Avant", 
+           stri_detect_fixed(choc, "Arrière") ~ "Arrière",
+           stri_detect_fixed(choc, "Côté") ~ "Côtés", 
+           stri_detect_fixed(choc, "multiples") ~ "Multiples"
+         ),
+         collision = case_when(
+           col == "Deux véhicules - frontale" ~ "Deux véhicules - frontale & autre",
+           col == "Autre collision" ~ "Deux véhicules - frontale & autre",
+           col == "Deux véhicules – par le coté" ~ "Deux véhicules",
+           col == "Deux véhicules – par l’arrière" ~ "Deux véhicules",
+           col == "Sans collision" ~ "Sans collision",
+           stri_detect_fixed(col, "Trois") ~ "Trois véhicules"
+         ),
+         cat_route = ifelse( 
+           test = catr %in% c("Hors réseau public", 
+                       "Parc de stationnement ouvert à la circulation publique", 
+                       "autre"),
+           yes = "Autre",
+           no = as.character(catr)
+         )
+         ) %>% 
+  select(-circ, -catv, -atm, -col, -catr)
+           
+         
+dim(global_ind)
+summary(global_ind)
 
 # write csv
 write.csv(global_ind, "outputs/individus_2017_alldata.csv")
+
+# check csv
+test <- read.csv("outputs/individus_2017_alldata.csv")
+
+
