2a9a587eaa4eada384dbd46dd0009f2142e52fc4
wanis-daoud <62149789+wanis-daoud@users.noreply.github.com>
2020-05-15 16:55:15 +0200

    Récupération des coordonées

--- r/data_theme_fct.R
+++ r/data_theme_fct.R
@@ -7,6 +7,10 @@ install.packages("dplyr") # dataset manipulation
 install.packages("kableExtra") # create nice html tables
 install.packages("stringi") # string manipulation
 install.packages("patchwork") # combine plots
+install.packages("DataExplorer") # explorateur de données
+install.packages("FactoMineR")# factorisation
+install.packages("Factoshiny")# ACM Intérface API
+
 
 
 library("corrplot") # corrélation
@@ -16,6 +20,8 @@ library("dplyr") # dataset manipulation
 library("kableExtra") # create nice html tables
 library("stringi") # string manipulation
 library("patchwork") # combine plots
+library("FactoMineR")# factorisation
+library("Factoshiny") # ACM Intérface API
 
 # Thème ggplot ------------------------------------------------------------
 
@@ -329,8 +335,10 @@ global_acc <- usag2017 %>%
 
 
 #----------------nbr et freq
+
 table(global_acc$sexe)
 prop.table(table(global_acc$sexe))
+
 
 #---------------pie ensemble
 colors<-c("blue","red","darkred")
@@ -363,11 +371,12 @@ ggplot(global_acc, aes(x = sexe , fill =sexe )) +
 #   scale_y_continuous(labels = scales::percent)
 
 #---------------Diagramme par gravité
-ggplot(global_acc, aes(x= sexe,  group=grav)) + 
+
+ggplot(global_acc, aes(x=sexe,  group=grav)) + 
   geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") +
   geom_text(aes( label = scales::percent(..prop..),
                  y= ..prop.. ), stat= "count", vjust = 1) +
-  labs(y = "Proportion", fill="Sexe") +
+  labs(y = "Proportion", fill="mod") +
   facet_grid(~grav) +
   scale_y_continuous(labels = scales::percent)
 
@@ -376,6 +385,185 @@ ggplot(global_acc, aes(x= sexe,  group=grav)) +
 sapply(global_acc, function(x) sum(!duplicated(x)))
 
 
+
+library(DataExplorer)
+
+create_report(global_acc)
+
+plot_intro(global_acc)
+plot_bar(global_acc$trajet)
+
+
+global_acc %>%
+  ggplot(aes(grav)) +
+  geom_bar(aes(fill = sexe))
+
+#recode variables
+
+table(global_acc$secu_rec)
+
+global_acc$trajet_rec<-
+  
+  global_acc$trajet_rec <- fct_recode(
+    global_acc$trajet,
+    "Pro" = "Domicile – travail",
+    "Pro" = "Utilisation professionnelle",
+    "Non Pro" = "Domicile – école",
+    "Non Pro" = "Courses – achats",
+    "Non Pro" = "Promenade – loisirs",
+    "Non Pro" = "Autre"
+  )
+
+global_acc$secu_rec <-
+  fct_recode(
+    global_acc$utilisation_equipement_secu,
+    "Oui" = "Oui",
+    "Non" = "Non",
+    NULL = "Non déterminable"
+  )
+
+global_acc$lum_rec <- fct_recode(
+  global_acc$lum,
+  "Jour" = "Plein jour",
+  "Nuit" = "Crépuscule ou aube",
+  "Nuit" = "Nuit sans éclairage public",
+  "Nuit" = "Nuit avec éclairage public non allumé",
+  "Nuit" = "Nuit avec éclairage public allumé"
+)
+
+global_acc$atm_rec <- fct_recode(
+  global_acc$atm,
+  "Condition météo normale" = "Normale",
+  "Mauvaise condition météo" = "Pluie légère",
+  "Mauvaise condition météo" = "Pluie forte",
+  "Mauvaise condition météo" = "Neige - grêle",
+  "Mauvaise condition météo" = "Brouillard - fumée",
+  "Mauvaise condition météo" = "Vent fort - tempête",
+  "Mauvaise condition météo" = "Temps éblouissant",
+  "Mauvaise condition météo" = "Temps couvert",
+  "Mauvaise condition météo" = "Autre"
+)
+                                 
+global_acc$catv_rec <- fct_recode(
+  global_acc$catv,
+  "Voiure" = "Voiture",
+  "Voiure" = "Véhicule utilitaire",
+  "Autres vehicule" = "Engin spécial",
+  "Autres vehicule" = "Tracteur",
+  "Autres vehicule" = "Transport en commun",
+  "Autres vehicule" = "Autre véhicule",
+  "Deux roues" = "Vélo",
+  "Deux roues" = "2 roues motorisé"
+)
+
+global_acc$surf_rec <- fct_recode(
+  global_acc$surf,
+  "Surface normale" = "Normale",
+  "Surface glissante" = "Mouillée",
+  "Surface glissante" = "Flaques",
+  "Surface glissante" = "Inondée",
+  "Surface glissante" = "Enneigée",
+  "Surface glissante" = "Boue",
+  "Surface glissante" = "Verglacée",
+  "Surface glissante" = "Corps gras - huile",
+  NULL = "Autre"
+  
+)
+
+#export excel
+
+write_xlsx(global_acc, "data/spss/global_acc.xlsx")
+
+#création dataset pour acm
+
+global_acc_f<-select(global_acc,Num_Acc,grav,groupe_age,sexe,zone,catu,agg,int,col,senc,obs,choc,manv,catr,prof,plan,trajet_rec,secu_rec,lum_rec,atm_rec,catv_rec,surf_rec)
+
+install.packages("gmodels")
+library(gmodels)
+CrossTable(global_acc$grav, global_acc$sexe)
+
+# for (i in 1:ncol(global_acc_f)){
+#   par(ask=T)
+#   plot(global_acc_f[,i])
+# }
+
+###ACM
+# res.mca<-MCA(global_acc_f[,2:ncol(global_acc_f)],quali.sup=2:4,graph=T,na.method="NA")
+res.mca<-MCA(global_acc_f[,2:ncol(global_acc_f)],quali.sup=1,graph=T,na.method="NA")
+
+# barplot(res.mca$eig[,2],names=paste("Dim",1:nrow(res.mca$eig)))
+
+###Représentation de l'inertie
+
+eig.val <- res.mca$eig
+barplot(eig.val[, 2], 
+        names.arg = 1:nrow(eig.val), 
+        main = "Variances Explained by Dimensions (%)",
+        xlab = "Principal Dimensions",
+        ylab = "Percentage of variances",
+        col ="steelblue")
+lines(x = 1:nrow(eig.val), eig.val[, 2], 
+      type = "b", pch = 19, col = "red")
+
+
+###Représentation des individus/modalités
+
+# plot(res.mca,
+#      invisible = c("var", "quali.sup"),
+#      autoLab = "yes")
+# 
+# plot(res.mca,
+#      invisible = c("ind", "quali.sup"),
+#      autoLab = "yes")
+
+#modalités+individus
+x11();plot(res.mca)
+#variables
+plot(res.mca,choix = "var",axes = 1:2)
+#modalités
+plot(res.mca, invisible = c("ind","quali.sup"),autoLab = "no",cex=0.7)
+plot(res.mca, invisible = c("ind"),autoLab = "no",cex=0.7,selectMod = "contrib 20")
+plot(res.mca, invisible = c("ind"),habillage=2,autoLab = "no",cex=0.7,selectMod = "contrib 20",axes = 3:4)
+
+#individus
+plot(res.mca, invisible = c("var","quali.sup"),habillage="grav")
+
+###Tableau d'inertie
+
+summary.MCA(res.mca,ncp=5,nbelements = 20)
+
+dimdesc(res.mca)
+
+
+###Récupération des coordonnées
+coord<-as.data.frame(res.mca$ind$coord[,1:5])
+global_acc_f<-cbind(global_acc_f,coord)
+
+
+
+
+# CrossTable(global_acc$grav, global_acc$agg)
+# 
+# table(global_acc$grav)
+# 
+# table(lieux$int)
+
+
+
+#Test Factoshiny
+
+# library(Factoshiny)
+# 
+# ourtmca<-MCAshiny(global_acc_f[,2:ncol(global_acc_f)])
+
+# 
+# 
+# table(global_acc$atm_rec)
+# table(global_acc$catv)
+# table(vehic2017$catv)
+# table(global_acc$catv_rec)
+# table(global_acc$surf_rec)
+# table(global_acc_f$utilisation_equipement_secu)
 
 # # dplyr
 # caract2017 %>% summarise_all(funs(n_distinct))
