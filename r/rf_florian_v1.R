7bcda5cbafe1bbfba129b67cbd7fff87239d5493
flods <florian.duchat@gmail.com>
2020-05-17 17:50:38 +0200

    Début Random Forest avant gestion des NA

---
+++ r/rf_florian_v1.R
@@ -0,0 +1,122 @@
+# Préparation de la base globale
+
+sapply(global_ind, function(x) sum(is.na(x)))
+sapply(global_ind, function(x) sum(is.na(x))/nrow(global_ind)*100)
+str(global_ind)
+table(global_ind$catu, global_ind$utilisation_equipement_secu, useNA = "always")
+dataflo <- global_ind %>% 
+        select(-Num_Acc, -lat, -long, -groupe_age, -dep)
+
+summary(dataflo)
+
+table(global_ind$plan)
+
+table(dataflo$grav)
+
+# Transformer les variables Quali en variables Quanti 
+library("caret")
+dummy_variables <- dummyVars(~., data= dataflo)
+dataflo_dichoto <- predict(dummy_variables, newdata= dataflo) # Le jeu de données s'est transformé en matrix car il n'y a plus que du numérique
+dataflo_dichoto <- as.data.frame(dataflo_dichoto) # Attention, la variable à expliquer est aussi dichotomisée
+dataflo_dichoto$"grav" <- ifelse(dataflo_dichoto$"grav.Indemne" == 1, "Indemne", 
+                          ifelse(dataflo_dichoto$"grav.Tué" == 1, "Tué", 
+                          ifelse(dataflo_dichoto$"grav.Blessé hospitalisé" == 1, "Blessé hospitalisé", 
+                          "Blessé léger"))) # Revenir à Grav initiale
+table(dataflo_dichoto$grav)
+# On supprime les colonnes inutiles
+dataflo_dichoto <- dataflo_dichoto %>% 
+      select(-"grav.Indemne", -"grav.Tué", -"grav.Blessé hospitalisé", -"grav.Blessé léger")
+
+dataflo_dichoto$grav <- as.factor(dataflo_dichoto$grav)
+
+# Nombre d'individus complètement renseignés (Aucune valeur manquante)
+nrow(na.omit(dataflo_dichoto))
+
+# Nombres de valeurs manquantes par variable
+library("VIM")
+summary(aggr(dataflo_dichoto, plot=FALSE))
+
+# Construire échantillon d'apprentissage et de Test
+set.seed(123)
+n <- nrow(dataflo_dichoto)
+perm<-sample(n) 
+dataflo_dichoto<-dataflo_dichoto[perm[1:n],] 
+
+# Tirage représentatif
+library(caret)
+tirage <- caret::createDataPartition(dataflo_dichoto$grav, p = 0.5, list = FALSE)
+global_ind_apprentissage <- dataflo_dichoto[tirage,]
+global_ind_test <- dataflo_dichoto[-tirage,]
+
+dim(global_ind_apprentissage)
+dim(global_ind_test)
+
+prop.table(table(global_ind_apprentissage$grav))*100
+prop.table(table(global_ind_test$grav))*100
+
+# MODELISATION AVEC RANDOM FOREST
+T1<-Sys.time()
+library("doParallel")
+cl <- makePSOCKcluster(4) 
+registerDoParallel(cl)
+# Fin Ouverture du cluster
+
+global_ind_apprentissage <- global_ind_apprentissage %>% 
+    select(catuConducteur, catuPassager, catuPiéton, sexe.Masculin, sexe.Féminin, grav, 
+           lumJour, "lumNuit avec éclairage",
+           "lumNuit avec éclairage",
+           "lumNuit sans éclairage",
+           "agg.Hors Agglo",
+           "agg.Agglo", 
+            "nb_pers_impliquées", "zoneIDF", "zoneProvince", 
+            "cat_vehic2 roues > 125 cm3")
+
+fitControl <- trainControl(method="LGOCV")
+
+rf1_model_500 <- caret::train(grav~., 
+                              data=global_ind_apprentissage,
+                              method = "ranger", 
+                              #preProcess = c("scale", "center"),
+                              #metric = "ROC",
+                              trControl = fitControl,
+                              #tuneGrid = rf_grid,
+                              num.trees = 500)
+# Fermeture du cluster
+on.exit(stopCluster(cl))
+T2<-Sys.time()
+# Fin Fermeture du cluster
+
+# Temps de calcul
+Tdiff <- difftime(T2, T1)
+print(Tdiff)
+
+print(rf1_model_500)
+pred <- predict(rf1_model_500,newdata=global_ind_test)
+print(table(pred))
+table(global_ind_test$grav)
+mat <- confusionMatrix(data=pred,reference=global_ind_test$grav)
+print(mat)
+
+
+# Brouillon
+56%
+
+rf_grid <- expand.grid(mtry = c(1, 2, 3, 4, 5, 6, 7),
+                       splitrule = c("gini", "extratrees"),
+                       min.node.size = c(1, 2, 5, 10, 20, 25, 30, 50))
+rf_grid
+
+
+
+fitControl <- caret::trainControl(classProbs=TRUE, 
+                                  summary=twoClassSummary,
+                                  sampling = "down",
+                                  method = "repeatedcv",
+                                  number = 8,
+                                  repeats=2)
+
+Prediction           Blessé hospitalisé Blessé léger Indemne   Tué
+Blessé hospitalisé               4637         2915     289   731
+Blessé léger                     2896         7204    2192   323
+Indemne                          5485        10375   22185   615
+Tué                                 6           10       4    18
