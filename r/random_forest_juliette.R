42a5b7af1b89e7d2416f9756055a7018ef12f26d
Juliette Bassnagel <juliette.bassnagel@mfglabs.com>
2020-05-17 15:56:58 +0200

    first commit rf

---
+++ r/random_forest_juliette.R
@@ -0,0 +1,10 @@
+
+# Librairies --------------------------------------------------------------
+
+library("dplyr") # dataset manipulation
+library("tidylog") # provide feedback about dplyr operations
+
+# Data --------------------------------------------------------------------
+
+base_modelisation <- read.csv("outputs/individus_2017_alldata.csv")
+test_data <- 
